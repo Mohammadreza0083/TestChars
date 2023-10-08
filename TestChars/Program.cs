59f29dc2659ee11228acc57f0a5a5f0c41432b97
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2023-10-09 00:31:40 +0330

    Final Program version

--- TestChars/Program.cs
+++ TestChars/Program.cs
@@ -2,7 +2,19 @@
     internal class Program {
         public static void Main(string[] args)
         {
-
+            System.Console.WriteLine("Welcome to the test program ...");
+            Console.WriteLine("Enter Brackets :");
+            string input = Console.ReadLine();
+            char[] bracket = input.ToCharArray();
+            FindBrackets findBrackets = new FindBrackets();
+            if (findBrackets.IsValidBrackets(bracket))
+            {
+                System.Console.WriteLine("true");
+            }
+            else
+            {
+                System.Console.WriteLine("false");
+            }
         }
     }
 }
